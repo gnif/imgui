b65c1d78f18878c9574f46990fbb2dd9f70bae8b
Ben Carter <ben@shironekolabs.com>
2020-04-24 19:30:20 +0200

    Atlas: Added font atlas API to request reloads.

--- imgui_draw.cpp
+++ imgui_draw.cpp
@@ -2000,9 +2000,8 @@ void    ImFontAtlas::ClearInputData()
             Fonts[i]->ConfigDataCount = 0;
         }
     ConfigData.clear();
-    CustomRects.clear();
-    PackIdMouseCursors = PackIdLines = -1;
     TexReady = false;
+    MarkDirty();
 }
 
 void    ImFontAtlas::ClearTexData()
@@ -2016,6 +2015,7 @@ void    ImFontAtlas::ClearTexData()
     TexPixelsRGBA32 = NULL;
     TexPixelsUseColors = false;
     // Important: we leave TexReady untouched
+    MarkDirty();
 }
 
 void    ImFontAtlas::ClearFonts()
@@ -2023,6 +2023,7 @@ void    ImFontAtlas::ClearFonts()
     IM_ASSERT(!Locked && "Cannot modify a locked ImFontAtlas between NewFrame() and EndFrame/Render()!");
     Fonts.clear_delete();
     TexReady = false;
+    MarkDirty();
 }
 
 void    ImFontAtlas::Clear()
@@ -2032,10 +2033,25 @@ void    ImFontAtlas::Clear()
     ClearFonts();
 }
 
+void    ImFontAtlas::MarkDirty()
+{
+    TexDirty = true;
+}
+
+void    ImFontAtlas::MarkClean()
+{
+    TexDirty = false;
+}
+
+bool    ImFontAtlas::IsDirty()
+{
+    return TexDirty;
+}
+
 void    ImFontAtlas::GetTexDataAsAlpha8(unsigned char** out_pixels, int* out_width, int* out_height, int* out_bytes_per_pixel)
 {
     // Build atlas on demand
-    if (TexPixelsAlpha8 == NULL)
+    if (TexPixelsAlpha8 == NULL || IsDirty())
         Build();
 
     *out_pixels = TexPixelsAlpha8;
@@ -2048,7 +2064,7 @@ void    ImFontAtlas::GetTexDataAsRGBA32(unsigned char** out_pixels, int* out_wid
 {
     // Convert to RGBA32 format on demand
     // Although it is likely to be the most commonly used format, our font rendering is 1 channel / 8 bpp
-    if (!TexPixelsRGBA32)
+    if (TexPixelsRGBA32 == NULL || IsDirty())
     {
         unsigned char* pixels = NULL;
         GetTexDataAsAlpha8(&pixels, NULL, NULL);
@@ -2097,6 +2113,7 @@ ImFont* ImFontAtlas::AddFont(const ImFontConfig* font_cfg)
     // Invalidate texture
     TexReady = false;
     ClearTexData();
+    MarkDirty();
     return new_font_cfg.DstFont;
 }
 
